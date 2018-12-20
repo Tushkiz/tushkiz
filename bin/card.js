9848aec0d499f90d97e488b95e64fef17641b317
Tushar Sonawane <tusharlsonawane@gmail.com>
2018-12-20 23:17:31 +0530

    update card

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Tushar Sonawane /'),
+  handle: chalk.cyan('tushkiz'),
+  work: chalk.white('Software Enginner at Vuclip'),
+  twitter: chalk.cyan('https://twitter.com/tushkiz'),
+  github: chalk.cyan('https://github.com/tushkiz'),
+  linkedin: chalk.cyan('https://linkedin.com/in/tushkiz'),
+  web: chalk.cyan('https://tushkiz.github.io'),
+  npx: chalk.white('npx tushkiz'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
